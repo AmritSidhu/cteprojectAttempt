fbf940fe41feb587590b559d8f80ab7c8780f3d7
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2017-11-22 16:25:11 +0000

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root   'home#index'
-get    '/help',    to: 'static_pages#help'
-get    '/about',   to: 'static_pages#about'
-get    '/signup',  to: 'users#new'
-get    '/login',   to: 'sessions#new'
-post   '/login',   to: 'sessions#create'
-delete '/logout',  to: 'sessions#destroy'
-resources :users
+   resources :users
+
+   root 'home#index'
+
+   get '/login', to: 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/logout' => 'sessions#destroy'
 
 end
